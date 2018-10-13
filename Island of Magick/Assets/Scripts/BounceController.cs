052c31e7c775fa238b84ef3ea78a54e9ad545331
VillePajala <valoraami@gmail.com>
2018-10-14 00:34:42 +0300

    Added Bouncing sound

---
+++ Island of Magick/Assets/Scripts/BounceController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceController : MonoBehaviour {
+
+    private AudioSource[] sounds = null;
+
+    void Start () {
+
+        this.sounds = GameObject.Find("AudioController").GetComponents<AudioSource>();
+
+    } // Start
+	
+	
+	void Update () {
+		
+	} // Update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Monster")
+        {
+            this.sounds[5].Play();
+        }
+    }
+}
