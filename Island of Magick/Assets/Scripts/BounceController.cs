c85d652ad9ec6997d8aed4d960f706f69025b7df
VillePajala <valoraami@gmail.com>
2018-10-18 13:46:49 +0300

    Added commenting to code and removed extra assets

--- Island of Magick/Assets/Scripts/BounceController.cs
+++ Island of Magick/Assets/Scripts/BounceController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class BounceController : MonoBehaviour {
 
+    // Creating the soundsource
     private AudioSource[] sounds = null;
 
     void Start () {
 
+        // Finding gameobjects
         this.sounds = GameObject.Find("AudioController").GetComponents<AudioSource>();
 
     } // Start
@@ -17,11 +19,16 @@ public class BounceController : MonoBehaviour {
 		
 	} // Update
 
+    // on collision
     private void OnCollisionEnter(Collision collision)
     {
+        // if the terrain is hit by a "monster"
         if(collision.gameObject.tag == "Monster")
         {
+            // Bouncing sound is played
             this.sounds[5].Play();
-        }
-    }
-}
+        } // if
+
+    } // OnCollisionEnter
+
+} // Class
--- Island of Magick/Assets/Scripts/CollisionController.cs
+++ Island of Magick/Assets/Scripts/CollisionController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CollisionController : MonoBehaviour {
 
+    // Defining gameobjects
     public GameObject exp2 = null;
-
+    // Soundsource
     private AudioSource[] sounds = null;
 
     void Start () {
 
+        // Finding gameobjects
         this.sounds = GameObject.Find("AudioController").GetComponents<AudioSource>();
 
     } // Start
@@ -19,12 +21,19 @@ public class CollisionController : MonoBehaviour {
 		
 	} // Update
 
+
+    // When this gameobject hits something (the bullet in this case)
     private void OnCollisionEnter(Collision collision)
     {
+        // sound of explosion is palyed
         this.sounds[4].Play();
+        // Explosion effect is created
         GameObject explosion2 = Instantiate(this.exp2, this.GetComponent<Transform>().position, Quaternion.identity);
+        // Explosion effect is destroyed after delay
         Destroy(explosion2, 8f);
+        // The bullet is destroyed on impact
         Destroy(gameObject);
-    }
+
+    } // OnCollisionEnter
 
 } // Class
